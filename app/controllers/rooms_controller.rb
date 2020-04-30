75a606ac5390ef5869aec72cd6fc85ef55b85486
glu000 <gl@gl.co.at>
2020-04-30 23:39:45 +0200

    Update rooms_controller.rb

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -164,7 +164,7 @@ class RoomsController < ApplicationController
     @room_settings = JSON.parse(@room[:room_settings])
     opts[:mute_on_start] = room_setting_with_config("muteOnStart")
     opts[:require_moderator_approval] = room_setting_with_config("requireModeratorApproval")
-        opts[:allow_recording] = room_settings["allowRecording"]
+    opts[:allow_recording] = room_settings["allowRecording"]
 
     begin
       redirect_to join_path(@room, current_user.name, opts, current_user.uid)
