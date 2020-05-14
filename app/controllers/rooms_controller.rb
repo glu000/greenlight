b026764a1800c57d8f1061b674711b8a1ddd4dde
glu000 <gl@gl.co.at>
2020-05-14 23:39:17 +0200

    Update rooms_controller.rb

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -48,10 +48,7 @@ class RoomsController < ApplicationController
     @room.room_settings = create_room_settings_string(room_params)
 
     upload_file = room_params[:upload_file]
-    upload_filename = room_params[:upload_filename]
-    if upload_filename
-       upload_filename.gsub(' ', '_')
-    end
+    upload_filename = room_params[:upload_filename].gsub(' ', '_')
 
     if upload_file
       room_name = ((@room.name).clone).gsub(" ", "_")
