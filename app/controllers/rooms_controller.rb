09ad58d630b030f7e8ef14e5351b4f6c4e2f6bf8
glu000 <gl@gl.co.at>
2020-05-16 08:45:34 +0200

    Update rooms_controller.rb

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -50,11 +50,11 @@ class RoomsController < ApplicationController
 
     logger.error ("Room Settings: " + room_settings_string)
 
-      #upload_file = params[:room][:upload_file]
-      #upload_filename = params[:room][:upload_filename].gsub(' ', '_')
+      upload_file = params[:room][:upload_file]
+      upload_filename = params[:room][:upload_filename].gsub(' ', '_')
 
-    upload_file = room_params[:upload_file]
-    upload_filename = room_params[:upload_filename]
+    #upload_file = room_params[:upload_file]
+    #upload_filename = room_params[:upload_filename]
 
     if upload_file
       room_name = ((@room.name).clone).gsub(" ", "_")
